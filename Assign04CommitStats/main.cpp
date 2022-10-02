01f4a6f73278d40d23a5a324bc3e7fd2d1578e15
Vonslonaker <Vonslonaker@DESKTOP-DJ8CHMR>
2022-10-02 11:24:54 -0700

    Add project files.

---
+++ Assign04CommitStats/main.cpp
@@ -0,0 +1,18 @@
+//*********************************************************************
+// File name:			main.cpp
+// Author:				
+// Date:					
+// Class:					
+// Assignment:		
+// Purpose:				
+// Hours Worked:	
+//*********************************************************************
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+	return EXIT_SUCCESS;
+}
